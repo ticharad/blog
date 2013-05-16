51076fd8206e6559a5f1bef068800738e02a7e9f
Tomás Charad <tomas.charad@gmail.com>
2013-05-15 22:55:11 -0400

    Starting with slides

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,31 @@
+//Sliders
+
+$(document).ready(function(){
+  $('.bxslider_horizontal').bxSlider({
+    minSlides: 1,
+    maxSlides: 1,
+    startSlide:1,
+    infiniteLoop: false,
+    hideControlOnEnd: true,
+    pager: false
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider_vertical').bxSlider({
+    minSlides: 1,
+    maxSlides: 1,
+    startSlide:0,
+    mode: 'vertical',
+    infiniteLoop: false,
+    hideControlOnEnd: true,
+    pager: false
+  });
+});
+
+//Adding classes to slider controls for identifying horizontal from vertical
+$(document).ready(function(){
+  $('.bx-wrapper').find('.bxslider_vertical').offsetParent().offsetParent().find('.bx-controls-direction').children().addClass('bxslider_vertical_controls')
+
+  $('.bx-wrapper').find('.bxslider_horizontal').parent().parent().children('.bx-controls').find('a').addClass('bxslider_horizontal_controls')
+});
